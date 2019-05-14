df3579a111144cfb7c981589edf346ac0046be04
github username <github email address>
2019-05-14 16:09:59 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  #Add an instance method #bark to your Dog class in
+  #lib/dog.rb that will puts "Woof!"
+
+  def bark
+    puts "Woof!"
+  end
+
+  #Add an instance method #sit to your Dog class in
+  #lib/dog.rb that will puts "The Dog is sitting".
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
